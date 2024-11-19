dbb669120b0be0b4c00f07d0431f2ca7282f79e7
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-11-19 11:50:55 +0600

    :heart_green: comments concepts in python programming

--- hello.py
+++ hello.py
@@ -5,3 +5,12 @@ print("Hello World 123 🚀")
 
 
 hello()
+
+#comments in python programming language
+# this is a single line comment
+
+"""_summary_
+    this is a multi line comment
+    These are also called doc strings
+    
+"""
