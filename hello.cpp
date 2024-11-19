0201b976d2bd19964b54b283337e25fcdcf78692
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-11-19 09:26:23 +0600

    write first program

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+    if(1) {
+        cout<< "valid number";
+
+        
+    }
+
+return 0;
+
+}
---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello World 122")
+def hello():
+    print("Hello World")
+print("Hello World 123 🚀")
+
+
+hello()
